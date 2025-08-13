3ed43777f12494ca9b4fbf8190176976408344e0
Rodrigo Correia <rodrigo.fc773@gmail.com>
2025-08-13 20:08:41 -0300

    first commit

---
+++ src/main/java/io/github/Rodfc773/rpg_references_api/RpgReferencesApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.Rodfc773.rpg_references_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgReferencesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgReferencesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Rodfc773/rpg_references_api/RpgReferencesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Rodfc773.rpg_references_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgReferencesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
