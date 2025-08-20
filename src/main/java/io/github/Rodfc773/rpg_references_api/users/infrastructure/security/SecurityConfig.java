c6e22b911e8d81a2e506a1aa2ca5e3c2cbb345ae
Rodrigo Correia <rodrigo.fc773@gmail.com>
2025-08-20 20:56:53 -0300

    Adicionando a camada de segurança para a rota de usuários

--- src/main/java/io/github/Rodfc773/rpg_references_api/users/infrastructure/security/SecurityConfig.java
+++ src/main/java/io/github/Rodfc773/rpg_references_api/users/infrastructure/security/SecurityConfig.java
@@ -1,4 +1,40 @@
 package io.github.Rodfc773.rpg_references_api.users.infrastructure.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private static final String[] PUBLIC_ROUTES = {
+            "api/v1/users/create",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
+    };
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        try{
+            http.csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(auth -> {
+                        auth.requestMatchers(PUBLIC_ROUTES).permitAll();
+                    });
+            return http.build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
