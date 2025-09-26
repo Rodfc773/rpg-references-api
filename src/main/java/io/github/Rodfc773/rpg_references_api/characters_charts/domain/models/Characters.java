6a3ae5c62936fe72f8c8bc4c971e7661be6cd897
Rodrigo Correia <rodrigo.fc773@gmail.com>
2025-09-26 19:30:59 -0300

    Criando a lógica dos personagens

---
+++ src/main/java/io/github/Rodfc773/rpg_references_api/characters_charts/domain/models/Characters.java
@@ -0,0 +1,47 @@
+package io.github.Rodfc773.rpg_references_api.characters_charts.domain.models;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+
+import java.util.UUID;
+
+@Entity
+public class Characters {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, name = "photo_url")
+    @NotBlank
+    private String name;
+
+    @Column(name = "photo_url")
+    private String profilePhotoUrl;
+
+    @Column(name = "level", nullable = false)
+    private int level;
+
+    private String alignment;
+
+    private String bloodType;
+
+    public Characters(){}
+
+    // GETTERS
+    public String getID() { return this.id.toString(); }
+    public String getName() { return this.name; }
+    public String getProfilePhotoUrl() {return this.profilePhotoUrl; }
+    public int getLevel() { return level;}
+    public String getAlignment() {return  this.alignment; }
+    public String getBloodType() { return this.bloodType; }
+
+    // SETTERS
+    public void setName(String name) { this.name = name; }
+    public void setProfilePhotoUrl(String profilePhotoUrl) { this.profilePhotoUrl = profilePhotoUrl; }
+    public void setLevel(int level) { this.level = level; }
+    public void setAlignment(String alignment) { this.alignment = alignment; }
+    public void setBloodType(String bloodType) { this.bloodType = bloodType; }
+}
--- src/main/java/io/github/Rodfc773/rpg_references_api/users/domain/exceptions/ErrorMessageDTO.java
+++ src/main/java/io/github/Rodfc773/rpg_references_api/users/domain/exceptions/ErrorMessageDTO.java
@@ -1,9 +1,14 @@
 package io.github.Rodfc773.rpg_references_api.users.domain.exceptions;
 
+import java.time.Instant;
+
 public class ErrorMessageDTO {
 
     private String message;
     private String field;
+    private Instant timeStamp;
+    private String path;
+    private int status;
 
     ErrorMessageDTO(String message, String field){
         this.message = message;
@@ -14,7 +19,19 @@ public class ErrorMessageDTO {
 
     public String getField(){ return this.field; }
 
+    public String getPath() { return  this.field; }
+
+    public int getStatus() { return this.status; }
+
+    public String getTimeStamp() { return this.timeStamp.toString(); }
+
     public void setMessage(String message) { this.message = message; }
 
     public void  setField(String field){ this.field = field; }
+
+    public void setTimeStamp() { this.timeStamp = Instant.now(); }
+
+    public void setPath(String path) { this.path = path; }
+
+    public void setStatus(int status) { this.status = status; }
 }
